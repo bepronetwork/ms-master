45c00127209c0fb62eb9d3460ed775cb2228b0c5
Ruiub3i <ruivaz9@hotmail.com>
2020-01-15 16:48:29 +0000

    hot fix getBets

--- src/db/repos/user.js
+++ src/db/repos/user.js
@@ -56,6 +56,7 @@ class UsersRepository extends MongoComponent{
     }
 
     getBets({id, size=15, dates, currency}){ 
+        console.log("dates ", dates, curency);
         try{
             return new Promise( (resolve, reject) => {
                 UsersRepository.prototype.schema.model
