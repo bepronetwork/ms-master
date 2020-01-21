edfac5b71718b4a0bb8ae0e38c455d9eeaa888d7
matheusrmeloo <matheusrodrigues030717@gmail.com>
2020-01-21 16:07:16 -0300

    Adding Migrations

---
+++ migrations/20200121022007-addTypographyToApp.js
@@ -0,0 +1,17 @@
+module.exports = {
+  async up(db, client) {
+    // TODO write your migration here.
+    // See https://github.com/seppevs/migrate-mongo/#creating-a-new-migration-script
+    // Example:
+    // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: true}});
+
+    let App = await db.collection('App').find({typography: { $exists:false }})
+    let typography = await db.collection('Typography').find();
+  },
+
+  async down(db, client) {
+    // TODO write the statements to rollback your migration (if possible)
+    // Example:
+    // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: false}});
+  }
+};
