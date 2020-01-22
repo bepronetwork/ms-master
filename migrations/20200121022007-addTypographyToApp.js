f7f0df04f9265397ad6ded1c25f2a00bec76fbeb
matheusrmeloo <matheusrodrigues030717@gmail.com>
2020-01-21 21:20:22 -0300

    Migration to Add Typography for App

--- migrations/20200121022007-addTypographyToApp.js
+++ migrations/20200121022007-addTypographyToApp.js
@@ -1,17 +1,11 @@
 module.exports = {
   async up(db, client) {
     // TODO write your migration here.
-    // See https://github.com/seppevs/migrate-mongo/#creating-a-new-migration-script
-    // Example:
-    // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: true}});
-
-    let App = await db.collection('App').find({typography: { $exists:false }})
-    let typography = await db.collection('Typography').find();
+    let typography = await db.collection('typographies').find({}).toArray();
+    await db.collection('apps').updateMany({typography:{$exists: false}}, {$set: {typography}});
   },
 
   async down(db, client) {
     // TODO write the statements to rollback your migration (if possible)
-    // Example:
-    // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: false}});
   }
 };
