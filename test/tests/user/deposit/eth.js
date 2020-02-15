43c2202a52be2c8887530fe47106e2b09d85f426
Paulo Henrique <paulohr.abreu@gmail.com>
2020-02-15 14:33:28 -0200

    modify test amount > Max deposit in APP

--- test/tests/user/deposit/eth.js
+++ test/tests/user/deposit/eth.js
@@ -78,15 +78,12 @@ context(`${ticker}`, async () => {
 
         let body = bitgoDepositExample();
         await DepositRepository.prototype.deleteDepositByTransactionHash(body.hash)
-        // Get User Deposit Address - create deposit address on bitgo
-        var res = await getDepositAddress({app : app.id, currency : currencyWallet.currency._id, id : user.id});
-        expect(res.data.status).to.equal(200);
-        expect(res.data.message.address).to.be.undefined;
+
         // Waiting 100 seconds for the address to be get intializaed
         console.log("Waiting for 3 minutes seconds for wallet init...");
         await delay(180*1000);
         // Get User Deposit Address - already initialized
-        res = await getDepositAddress({app : app.id, currency : currencyWallet.currency._id, id : user.id});
+        let res = await getDepositAddress({app : app.id, currency : currencyWallet.currency._id, id : user.id});
         expect(res.data.status).to.equal(200);
         expect(res.data.message.address).to.not.be.null;
         expect(res.data.status).to.equal(200);
