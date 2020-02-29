827e3de7b0261b98056ac2b11ecfff7c0f3520a4
Paulo Henrique <paulohr.abreu@gmail.com>
2020-02-29 18:17:32 -0300

    update docs

--- scripts/pre-push-check.sh
+++ scripts/pre-push-check.sh
@@ -3,7 +3,6 @@ set -e
 branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 if [ "$branch" == "issue-81" ]
 then
-    # sudo npm install -g npx
     sudo npm i docsify-cli@4.4.0 -g
     npm run run-doc
     date > time.tm
