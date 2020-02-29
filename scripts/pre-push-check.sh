ec8b939d16f8f57e5be935f3c943b56d68bef306
Paulo Henrique <paulohr.abreu@gmail.com>
2020-02-29 17:42:35 -0300

    update document

--- scripts/pre-push-check.sh
+++ scripts/pre-push-check.sh
@@ -3,10 +3,10 @@ set -e
 branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 if [ "$branch" == "issue-81" ]
 then
-    sudo npm install -g npx
-    sudo npm i docsify-cli -g
-    sudo npm run heroku-doc
-    date > time.tm
+    # sudo npm install -g npx
+    # sudo npm i docsify-cli -g
+    # sudo npm run heroku-doc
+    # date > time.tm
     git add .
     git commit -am 'update document'
 fi
