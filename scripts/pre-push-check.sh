f910745c588544132f4c0cb76ad6251a3aee261f
Paulo Henrique <paulohr.abreu@gmail.com>
2020-02-29 17:52:03 -0300

    update document

--- scripts/pre-push-check.sh
+++ scripts/pre-push-check.sh
@@ -4,7 +4,7 @@ branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 if [ "$branch" == "issue-81" ]
 then
     # sudo npm install -g npx
-    # sudo npm i docsify-cli -g
+    sudo npm i docsify-cli -g
     npm run run-doc
     date > time.tm
     git add .
