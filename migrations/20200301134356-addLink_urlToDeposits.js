3cccdc29441bcf78d5a960264ccc9be408685c86
matheusrmeloo <matheusrodrigues030717@gmail.com>
2020-03-02 13:08:54 -0300

    Adding Migration to Link_url of deposits

--- migrations/20200301134356-addLink_urlToDeposits.js
+++ migrations/20200301134356-addLink_urlToDeposits.js
@@ -1,9 +1,53 @@
+'use strict'
+require('dotenv').config();
+
+const setLinkUrl = ({ ticker, address }) => {
+  var link_url;
+  if (process.env.ENV === "development") {
+    switch (new String(ticker).toLowerCase().trim()) {
+      case 'eth': {
+        link_url = `https://kovan.etherscan.io/tx/${address}`;
+        return link_url;
+      };
+      default:
+        link_url = address;
+        return link_url;
+    }
+  } else if (process.env.ENV === "production") {
+    switch (new String(ticker).toLowerCase().trim()) {
+      case 'eth': {
+        link_url = `https://etherscan.io/tx/${address}`;
+        return link_url;
+      };
+      default:
+        link_url = address;
+        return link_url;
+    }
+  }
+}
+
 module.exports = {
   async up(db, client) {
-    // TODO write your migration here.
-    // See https://github.com/seppevs/migrate-mongo/#creating-a-new-migration-script
-    // Example:
-    // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: true}});
+    let deposits = await db.collection('deposits').find().sort({currency: -1}).limit(10).toArray();
+    for (let deposit of deposits) {
+      var address = '0X';
+      var ticker = 'ETH';
+      let currency = await db.collection('currencies').findOne({ _id: deposit.currency })
+      try {
+        ticker = currency.ticker
+      } catch (error) {
+        ticker = 'ETH'
+      }
+      if (deposit.transactionHash != undefined) {
+        address = deposit.transactionHash
+      }
+      if (deposit.link_url == undefined) {
+        var link_url = setLinkUrl({ address, ticker });
+        await db.collection('deposits').updateOne(
+          { _id: deposit._id },
+          { $set: { "link_url": link_url } });
+      }
+    }
   },
 
   async down(db, client) {
