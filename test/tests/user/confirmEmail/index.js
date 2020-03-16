142372fb3fc49461ca92fe9a458db4cc5a97541b
Paulo Henrique <paulohr.abreu@gmail.com>
2020-03-16 00:24:18 -0300

    data to resend email test

--- test/tests/user/confirmEmail/index.js
+++ test/tests/user/confirmEmail/index.js
@@ -48,7 +48,7 @@ context('Confirm Email', async () => {
     }));
 
     it('should resend email to confirm', mochaAsync(async () => {
-        const res = resendEmail({user: user.data.message.id}, user.data.message.bearerToken, {id : user.data.message.id});
+        const res = resendEmail({user: user.id}, user.bearerToken, {id : user.id});
         expect(res.data.status).to.not.null;
         expect(res.data.status).to.equal(200);
     }));
