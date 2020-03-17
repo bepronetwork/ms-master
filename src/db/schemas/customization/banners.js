ffe2c661bdd24523fa09b90cfd907bb1a4cd8e97
Paulo Henrique <paulohr.abreu@gmail.com>
2020-03-17 20:44:43 -0300

    new filds to banner

--- src/db/schemas/customization/banners.js
+++ src/db/schemas/customization/banners.js
@@ -7,15 +7,17 @@ BannersSchema.prototype.name = 'Banners';
 
 BannersSchema.prototype.schema =  {
     ids                     : [{
-        image_url : {type : String}, 
-        link_url  : {type: String}
+        image_url   : {type : String},
+        link_url    : {type : String},
+        button_text : {type : String},
+        title       : {type : String},
+        subtitle    : {type : String}
     }],
-    autoDisplay             : { type : Boolean, default : false}
+    autoDisplay : { type : Boolean, default : false}
 }
 
 
 BannersSchema.prototype.model = db.model(BannersSchema.prototype.name, new db.Schema(BannersSchema.prototype.schema));
-      
 export {
     BannersSchema
 }
