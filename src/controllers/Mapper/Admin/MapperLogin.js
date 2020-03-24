abb03f3da1ea82a36cd800d0254be1f88c4b5edd
matheusrmeloo <matheusrodrigues030717@gmail.com>
2020-03-24 13:59:14 -0300

    Another chance with security now :)

--- src/controllers/Mapper/Admin/MapperLogin.js
+++ src/controllers/Mapper/Admin/MapperLogin.js
@@ -23,7 +23,7 @@ let outputs = {
             "name": object.name,
             "hash_password": object.hash_password,
             "security": {
-                "id"                    : object.security._id,
+                "_id"                    : object.security._id,
                 "2fa_set"               : object.security['2fa_set'],
                 "email_verified"        : object.security['email_verified'],
                 "bearerToken"           : object.security['bearerToken'],
