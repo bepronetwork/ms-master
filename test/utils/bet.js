8075b1f7191e392d77ee3da6a0f5a1bc055b697c
Paulo Henrique <paulohr.abreu@gmail.com>
2020-03-28 08:51:51 -0300

    loss amount in jackpot to test

--- test/utils/bet.js
+++ test/utils/bet.js
@@ -2,7 +2,7 @@ import chai from 'chai';
 const expect = chai.expect;
 
 export async function digestBetResult({newBalance, res, previousBalance, percentageJackpot=1}){
-    const { winAmount, betAmount, fee, isWon, outcomeResultSpace, result, user_delta, jackpot, lossAmount} = res.data.message;
+    const { winAmount, betAmount, fee, isWon, outcomeResultSpace, result, user_delta, jackpot} = res.data.message;
     console.log(winAmount, isWon, user_delta, newBalance, previousBalance)
     if(isWon){
         // Confirm delta is positive
@@ -12,7 +12,7 @@ export async function digestBetResult({newBalance, res, previousBalance, percent
         // Confirm New User Balance is equal to previous plus delta
         expect(newBalance).to.be.equal(previousBalance+user_delta+jackpot.user_delta);
         // Check if wagered value in jackpot is n% edge limited per jackpot
-        expect(betAmount*percentageJackpot*0.01).to.be.equal(lossAmount);
+        expect(betAmount*percentageJackpot*0.01).to.be.equal(jackpot.lossAmount);
     }else{
         // Confirm delta is negative
         expect(user_delta).to.be.lessThan(0);
@@ -21,7 +21,7 @@ export async function digestBetResult({newBalance, res, previousBalance, percent
         // Confirm New User Balance is equal to previous plus delta
         expect(newBalance).to.be.equal(previousBalance+user_delta+jackpot.user_delta);
         // Check if wagered value in jackpot is n% edge limited per jackpot
-        expect(betAmount*percentageJackpot*0.01).to.be.equal(lossAmount);
+        expect(betAmount*percentageJackpot*0.01).to.be.equal(jackpot.lossAmount);
     }
     return true;
 }
