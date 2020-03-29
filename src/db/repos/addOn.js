4e052a06b5048790e3b7de71de93f22258f1f2ed
Paulo Henrique <paulohr.abreu@gmail.com>
2020-03-29 02:27:10 -0300

    async function erro add jackpot

--- src/db/repos/addOn.js
+++ src/db/repos/addOn.js
@@ -36,7 +36,7 @@ class AddOnRepository extends MongoComponent{
                     $set: {jackpot}
                 }
             )
-            .exec( (err, item) => {
+            .exec( async (err, item) => {
                 if(err){reject(err)}
                 resolve(item);
             });
