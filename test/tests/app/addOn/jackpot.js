6e472c57b82d8fcb31a584be53f07181acf50c53
Paulo Henrique <paulohr.abreu@gmail.com>
2020-03-31 14:42:42 -0300

    test jackpot edit edge

--- test/tests/app/addOn/jackpot.js
+++ test/tests/app/addOn/jackpot.js
@@ -1,6 +1,6 @@
 import chai from 'chai';
 import { mochaAsync, detectValidationErrors } from '../../../utils';
-import { addJackpot } from '../../../methods';
+import { addJackpot, editEdgeJackpot} from '../../../methods';
 import { get_app } from '../../../models/apps';
 const expect = chai.expect;
 
@@ -16,6 +16,20 @@ context('Jackpot', async () => {
         let res = await addJackpot({app: app.id, admin: admin.id}, admin.security.bearerToken , {id : admin.id});
         expect(detectValidationErrors(res)).to.be.equal(false);
         const { status } = res.data;
+        expect(status).to.be.equal(200);
+    }));
+
+    it('should modify edge jackpot to 2', mochaAsync(async () => {
+        let res = await editEdgeJackpot({ edge: 2, app: app.id, admin: admin.id}, admin.security.bearerToken , {id : admin.id});
+        expect(detectValidationErrors(res)).to.be.equal(false);
+        const { status} = res.data;
+        expect(status).to.be.equal(200);
+    }));
+
+    it('should modify edge jackpot to 1', mochaAsync(async () => {
+        let res = await editEdgeJackpot({ edge: 1, app: app.id, admin: admin.id}, admin.security.bearerToken , {id : admin.id});
+        expect(detectValidationErrors(res)).to.be.equal(false);
+        const { status} = res.data;
         expect(status).to.be.equal(200);
     }));
 });
