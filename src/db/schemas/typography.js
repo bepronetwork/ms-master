b93d1aa79c658ee9d925865cb16117b149a8472d
Paulo Henrique <paulohr.abreu@gmail.com>
2020-04-04 09:52:39 -0300

    add default typography

--- src/db/schemas/typography.js
+++ src/db/schemas/typography.js
@@ -6,8 +6,8 @@ class TypographySchema{};
 TypographySchema.prototype.name = 'Typography';
 
 TypographySchema.prototype.schema =  {
-    url  : { type : String, default: '' },
-    name : { type : String, default: '' }
+    url  : { type : String, default: 'https://fonts.gstatic.com/s/opensans/v16/mem8YaGs126MiZpBA-UFWJ0bf8pkAp6a.woff2' },	    url  : { type : String, default: '' },
+    name : { type : String, default: 'OpenSans-Regular' }
 }
 
 TypographySchema.prototype.model = db.model(TypographySchema.prototype.name, new db.Schema(TypographySchema.prototype.schema));
