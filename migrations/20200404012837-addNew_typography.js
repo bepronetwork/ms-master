13e32ac049798c859435c7f6a56251ef62105573
Paulo Henrique <paulohr.abreu@gmail.com>
2020-04-03 22:59:25 -0300

    migration typography

---
+++ migrations/20200404012837-addNew_typography.js
@@ -0,0 +1,43 @@
+class Progress {
+
+  constructor(value, name) {
+      this.progress = value;
+      this.name     = name;
+      this.objProgress = setInterval(()=>{
+        console.clear(); console.log(this.name);
+        console.log(`${this.progress} process left`);
+      }, 2000);
+  }
+
+  setProcess(value) {
+      this.progress = value;
+  }
+
+  destroyProgress() {
+      clearInterval(this.objProgress);
+  }
+
+}
+
+module.exports = {
+  async up(db, client) {
+
+    let apps = await db.collection('apps').find().toArray();
+    let processIndex  = apps.length;
+    let processObj    = new Progress(processIndex, "ADD_NEW_TYPOGRAPHY");
+    for(let app of apps) {
+      processObj.setProcess(processIndex);
+      processIndex --;
+      let result = await db.collection('typographies').insertOne({url: '', name: '', __v : 0});
+      await db.collection('apps').updateOne(
+        { _id: app._id },
+        { $set: { "typography": result.ops[0]._id } }
+      )
+
+    }
+    processObj.destroyProgress();
+  },
+
+  async down(db, client) {
+  }
+};
