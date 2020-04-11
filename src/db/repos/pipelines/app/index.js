fbd931c980146c83c25180b1ff6958e650728d36
matheusrmeloo <matheusrodrigues030717@gmail.com>
2020-04-11 10:29:24 -0300

    Change to another files

--- src/db/repos/pipelines/app/index.js
+++ src/db/repos/pipelines/app/index.js
@@ -5,10 +5,8 @@ import pipeline_game_stats from "./game_stats";
 import pipeline_app_wallet from "./app_wallet";
 import pipeline_get_by_external_id from './get_by_external_id';
 import pipeline_last_bets from './last_bets';
-import { 
-    pipeline_biggest_user_winners_by_currency,
-    pipeline_biggest_user_winners_all_currency
-} from './biggest_user_winners';
+import { pipeline_biggest_user_winners_all_currency } from './biggest_user_winners';
+import { pipeline_biggest_user_winners_by_currency } from "./biggest_user_winners_by_currency";
 import pipeline_biggest_bet_winners from './biggest_bet_winners';
 import pipeline_popular_numbers from './popular_numbers';
 
@@ -17,11 +15,11 @@ export {
     pipeline_popular_numbers,
     pipeline_revenue_stats,
     pipeline_user_stats,
-    pipeline_biggest_user_winners_by_currency,
     pipeline_biggest_bet_winners,
     pipeline_game_stats,
     pipeline_app_wallet,
     pipeline_get_by_external_id,
     pipeline_last_bets,
-    pipeline_biggest_user_winners_all_currency
+    pipeline_biggest_user_winners_all_currency,
+    pipeline_biggest_user_winners_by_currency
 }
--- src/db/repos/pipelines/filters/index.js
+++ src/db/repos/pipelines/filters/index.js
@@ -1,8 +1,10 @@
 import { pipeline_bets_by_date } from './by_date';
 import { pipeline_bets_by_currency } from './by_currency';
+import { pipeline_match_by_currency } from "./match_by_currency";
 
 
 export {
     pipeline_bets_by_date,
-    pipeline_bets_by_currency
+    pipeline_bets_by_currency,
+    pipeline_match_by_currency
 }
