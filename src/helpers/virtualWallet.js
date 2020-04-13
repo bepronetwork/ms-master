a7a164f5f2a7cc8f2becb214f2dd4743a040f2b9
Ruiub3i <ruivaz9@hotmail.com>
2020-04-13 17:57:12 +0100

    hot-fix

--- src/helpers/virtualWallet.js
+++ src/helpers/virtualWallet.js
@@ -1,6 +1,10 @@
 import { compareCurrencyId } from "./currency";
 
 export function getVirtualAmountFromRealCurrency({currency, virtualWallet, currencyAmount}){
-    const priceObject = virtualWallet.price.find( c => compareCurrencyId(c, currency._id));
+    console.log("virtual Wallet", virtualWallet.price);
+    console.log("Currency", currency);
+    console.log("Currency Amount", currencyAmount);
+
+    const priceObject = virtualWallet.price.find( p => compareCurrencyId(p.currency, currency._id));
     return parseFloat(parseFloat(priceObject.amount) * parseFloat(currencyAmount))
 }
