528789f6432eda4f32778d8148c28a0b41c82bcc
matheusrmeloo <matheusrodrigues030717@gmail.com>
2020-04-25 17:12:50 -0300

    Deleting Console

--- test/tests/admin/register/index.js
+++ test/tests/admin/register/index.js
@@ -159,7 +159,6 @@ context('Register', async () => {
 
     it('should get All App Bets', mochaAsync(async () => {
         let res = await getAppUsersBets({"app": "5e48d6a928c1af0021c366d6", "admin": "5e48d69228c1af0021c366c2"}, "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IkF1dGgvNWU0OGQ2OTIyOGMxYWYwMDIxYzM2NmMyIiwidGltZSI6MTU4OTIzNTk4NzEzOSwiaWF0IjoxNTg2NjQzOTg3fQ.oo_fjK3BaD98Gl6xQFgw3zeyQL9GnIQhPInq_MFUDs_yMsNaDYYvmjAwvf3n21w1DOhHAA8FcVk76blWrixKjw", {"id" : "5e48d69228c1af0021c366c2"});
-        console.log(res.data.message.list[0])
         detectValidationErrors(res);
         expect(res.data.status).to.not.null;
         expect(res.data.status).to.equal(200);
