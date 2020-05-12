a9fe3173c18ee52132dd68735e66a7ea6f373dc8
matheusrmeloo <matheusrodrigues030717@gmail.com>
2020-05-11 22:56:00 -0300

    Testando If

--- src/api/helpers/file.js
+++ src/api/helpers/file.js
@@ -9,6 +9,7 @@ const FOLDER_NAME = 'test/outputs';
 export const writeFile = ({functionName, content}) => {
     /* To output the information to confirm no information leaks happen passphrases and passwords */
     if(IS_LOCAL_DEV && IS_TEST){
+        console.log("Entrouu")
         fs.writeFileSync(`${FOLDER_NAME}/${functionName}.json`, JSON.stringify(content));
     }else{
         // To not write in production
