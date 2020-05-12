93c797047e2050ca78b3933cba0a9955a04ddb8f
matheusrmeloo <matheusrodrigues030717@gmail.com>
2020-05-11 23:04:09 -0300

    Deleting Consoles

--- src/api/helpers/file.js
+++ src/api/helpers/file.js
@@ -1,15 +1,11 @@
 import fs from 'fs';
 import { IS_LOCAL_DEV, IS_TEST } from '../../config';
 
-console.log(IS_LOCAL_DEV)
-console.log(IS_TEST)
-
 const FOLDER_NAME = 'test/outputs';
 
 export const writeFile = ({functionName, content}) => {
     /* To output the information to confirm no information leaks happen passphrases and passwords */
     if(IS_LOCAL_DEV == "true" && IS_TEST == "true"){
-        console.log("Entrouu")
         fs.writeFileSync(`${FOLDER_NAME}/${functionName}.json`, JSON.stringify(content));
     }else{
         // To not write in production
