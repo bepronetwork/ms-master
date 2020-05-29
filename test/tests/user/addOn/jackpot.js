467d7594eeb030bd05e6064c8f320ac3414092c0
Paulo Henique R Abreu <paulohrodriguesaifal@gmail.com>
2020-05-28 21:17:08 -0300

    resolve problem test get jackpot

--- test/tests/user/addOn/jackpot.js
+++ test/tests/user/addOn/jackpot.js
@@ -1,8 +1,8 @@
 import chai from 'chai';
-import { mochaAsync, detectValidationErrors } from '../../../utils';
-import { getPotJackpot } from '../../../methods';
+import { mochaAsync } from '../../../utils';
+import { getPotJackpot, getAppAuth } from '../../../methods';
 const expect = chai.expect;
-
+const ticker = 'ETH';
 context('Jackpot', async () => {
     var app, admin, user, currency;
 
@@ -10,12 +10,12 @@ context('Jackpot', async () => {
         app      = global.test.app;
         admin    = global.test.admin;
         user     = global.test.user;
-        let appWithWallet = (await getAppAuth({app : admin.app.id, admin: admin.id}, admin.security.bearerToken, {id : admin.id})).data.message;
+        let appWithWallet = (await getAppAuth({app : app.id, admin: admin.id}, admin.security.bearerToken, {id : admin.id})).data.message;
         currency = (appWithWallet.wallet.find( w => new String(w.currency.ticker).toLowerCase() == new String(ticker).toLowerCase())).currency;
     });
 
     it('should get pot jackpot', mochaAsync(async () => {
-        const res = await getPotJackpot({ currency, app: app.id, user: user.id}, user.bearerToken , {id : user.id});
+        const res = await getPotJackpot({ currency: currency._id, app: app.id, user: user.id}, user.bearerToken , {id : user.id});
         const { status } = res.data;
         expect(status).to.be.equal(200);
     }));
