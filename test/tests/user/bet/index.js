9defce114278bffe51d098abb5a54b1fda563e71
matheusrmeloo <matheusrodrigues030717@gmail.com>
2020-06-05 00:16:14 -0300

    trying to see bets deposit bonus

--- test/tests/user/bet/index.js
+++ test/tests/user/bet/index.js
@@ -1,9 +1,9 @@
 context('Bets', async () => {
     // require('./keno_simple');
-    require('./coinflip');
-    require('./european_roulette');
-    require('./linear_dice');
-    require('./errors');
-    require('./jackpot');
+    // require('./coinflip');
+    // require('./european_roulette');
+    // require('./linear_dice');
+    // require('./errors');
+    // require('./jackpot');
     require('./depositBonus');
 });
