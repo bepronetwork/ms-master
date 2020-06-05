96694c4a5fbf599eb146a562adbde3dab397f4a0
matheusrmeloo <matheusrodrigues030717@gmail.com>
2020-06-05 11:47:53 -0300

    Trying to see test

--- test/tests/user/bet/index.js
+++ test/tests/user/bet/index.js
@@ -1,9 +1,10 @@
 context('Bets', async () => {
     // require('./keno_simple');
-    // require('./coinflip');
-    // require('./european_roulette');
-    // require('./linear_dice');
-    // require('./errors');
-    // require('./jackpot');
+    require('./depositBonus');
+    require('./coinflip');
+    require('./european_roulette');
+    require('./linear_dice');
+    require('./errors');
+    require('./jackpot');
     require('./depositBonus');
 });
