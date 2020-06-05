7afd2246b989ae393f1ebe5456a65f95d7f03a20
matheusrmeloo <matheusrodrigues030717@gmail.com>
2020-06-05 16:52:28 -0300

    See consoles

--- test/utils/bet.js
+++ test/utils/bet.js
@@ -4,12 +4,45 @@ const expect = chai.expect;
 
 
 export async function digestBetResult({newBalance, res, previousBalance, edge, previousBalanceApp, newBalanceApp}){
+    console.log("res.data.message::  ",res.data.message)
     const { winAmount, betAmount, fee, isWon, outcomeResultSpace, result, user_delta, app_delta, jackpotAmount, totalBetAmount, affiliateReturns } = res.data.message;
     const totalAffiliateAmount = affiliateReturns.reduce( (acc, a) => parseFloat(a._id.amount)+acc, 0);
     if(affiliateReturns[0]){
         console.log("totalAffiliateAmount", affiliateReturns[0], totalAffiliateAmount)
     }
     console.log("a", betAmount, totalBetAmount, jackpotAmount, fee, edge, user_delta, totalAffiliateAmount, previousBalanceApp, newBalanceApp, newBalance, previousBalance);
+    /* Verify if Fee is right */
+    expect(Numbers.toFormatBet(fee)).to.be.equal(Numbers.toFormatBet(totalBetAmount*(edge/100)))
+    /* Verify if Jackpot Value is right */
+    expect(Numbers.toFormatBet(totalBetAmount)).to.be.equal(Numbers.toFormatBet(betAmount + jackpotAmount + fee ))
+    
+    if(isWon){
+        /* Verify if WinAmount is ight Value is right */
+        expect(winAmount).to.be.greaterThan(0)
+        // Confirm New User Balance is equal to previous plus delta
+        expect(Numbers.toFormatBet(newBalance)).to.be.equal(Numbers.toFormatBet(previousBalance + user_delta));
+    }else{
+        // Confirm delta is negative
+        expect(Numbers.toFormatBet(user_delta)).to.be.lessThan(0)
+        expect(Numbers.toFormatBet(user_delta)).to.be.equal(Numbers.toFormatBet(-(betAmount + fee + jackpotAmount)));
+        // Confirm Win Amount is 0
+        expect(winAmount).to.be.equal(0);
+    }
+
+    // Confirm app and user deltas are opposite
+    expect(Numbers.toFormatBet(app_delta)).to.be.equal(Numbers.toFormatBet(-(user_delta+totalAffiliateAmount)));
+    // Confirm New User Balance is equal to previous plus delta
+    expect(Numbers.toFormatBet(newBalance)).to.be.equal(Numbers.toFormatBet(previousBalance+user_delta));
+    // Confim balance of App to be the diff
+    expect(Numbers.toFormatBet(newBalanceApp)).to.be.equal(Numbers.toFormatBet(previousBalanceApp+app_delta));
+
+    return true;
+}
+
+export async function digestBetBonusResult({newBalance, res, previousBalance, previousBalanceApp, newBalanceApp, previousBonusBalance, newBonusBalance}){
+    const { winAmount, betAmount, fee, isWon, user_delta, app_delta, jackpotAmount, totalBetAmount, affiliateReturns } = res.data.message;
+    const totalAffiliateAmount = affiliateReturns.reduce( (acc, a) => parseFloat(a._id.amount)+acc, 0);
+    console.log("a", betAmount, totalBetAmount, jackpotAmount, fee, edge, user_delta, totalAffiliateAmount, previousBalanceApp, newBalanceApp, newBalance, previousBalance, previousBonusBalance, newBonusBalance);
     /* Verify if Fee is right */
     expect(Numbers.toFormatBet(fee)).to.be.equal(Numbers.toFormatBet(totalBetAmount*(edge/100)))
     /* Verify if Jackpot Value is right */
