c3970d0cc93eaf2aa241347d07ff1435454db24c
matheusrmeloo <matheusrodrigues030717@gmail.com>
2020-06-07 15:32:03 -0300

    Understanding Error

--- test/tests/user/bet/depositBonus.js
+++ test/tests/user/bet/depositBonus.js
@@ -66,9 +66,9 @@ context('After Deposit Bonus sBets (Overall Math)', async () => {
         betAmount = 0.000001;
         admin = (await authAdmin({ admin : global.test.admin.id }, global.test.admin.security.bearerToken, { id : global.test.admin.id})).data.message;
         console.log("admin:: ",admin)
-        await loginUser({username : user.username, password : user.password, app : app._id}).data.message;
-        user = (await getUserAuth({user : user._id, app: app._id}, user.bearerToken, {id : user._id})).data.message;
-        app = (await getAppAuth({app : admin.app.id, admin: admin.id}, admin.security.bearerToken, {id : admin.id})).data.message;
+        await loginUser({username : user.username, password : user.password, app : app.id}).data.message;
+        user = (await getUserAuth({user : user.id, app: app.id}, user.bearerToken, {id : user.id})).data.message;
+        app = (await getAppAuth({app : app.id, admin: admin.id}, admin.security.bearerToken, {id : admin.id})).data.message;
         currency = (app.wallet.find( w => new String(w.currency.ticker).toLowerCase() == new String(ticker).toLowerCase())).currency;
         walletApp = (app.wallet.find( w => new String(w.currency.ticker).toLowerCase() == new String(ticker).toLowerCase()));
         userWallet = (user.wallet.find( w => new String(w.currency.ticker).toLowerCase() == new String(ticker).toLowerCase()));
