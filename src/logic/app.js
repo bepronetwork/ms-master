cc231cad3c739a4a660783807e87ec89937f8f7d
Paulo Henique R Abreu <paulohrodriguesaifal@gmail.com>
2020-06-09 10:56:30 -0300

    nodify playbalance user

--- src/logic/app.js
+++ src/logic/app.js
@@ -127,6 +127,19 @@ const processActions = {
         }
 		return normalized;
     },
+    __modifyBalance : async (params) => {
+        try {
+            let app = await AppRepository.prototype.findAppById(params.app, "simple");
+            if(!app){throwError('APP_NOT_EXISTENT');}
+            let user = await UsersRepository.prototype.findUserById(params.user, "simple");
+            if(user.app_id != app._id){throwError('USER_NOT_EXISTENT_IN_APP');}
+            let wallet = user.wallet.find( w => new String(w._id).toString() == new String(params.wallet).toString());
+            if(!wallet){throwError('CURRENCY_NOT_EXISTENT');}
+            return params;
+        } catch(err) {
+            throw err;
+        }
+    },
     __getLogs : async (params) => {
         try {
             let app = await AppRepository.prototype.findAppById(params.app, "simple");
@@ -745,6 +758,10 @@ const progressActions = {
     },
     __appGetUsersBets : async (params) => {
         return params;
+    },
+    __modifyBalance : async (params) => {
+        await WalletsRepository.prototype.updatePlayBalanceNotInc(params.wallet, {newBalance : params.newBalance});
+        return true;
     },
     __getLogs : async (params) => {
         return params;
@@ -1538,6 +1555,9 @@ class AppLogic extends LogicComponent{
                 case 'EditBackground' : {
 					return await library.process.__editBackground(params); break;
                 };
+                case 'ModifyBalance' : {
+					return await library.process.__modifyBalance(params); break;
+                };
 			}
 		}catch(error){
 			throw error
@@ -1700,6 +1720,9 @@ class AppLogic extends LogicComponent{
                 }
                 case 'EditBackground': {
                     return await library.progress.__editBackground(params); break;
+                }
+                case 'ModifyBalance': {
+                    return await library.progress.__modifyBalance(params); break;
                 }
 			}
 		}catch(error){
