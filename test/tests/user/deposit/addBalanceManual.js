530e5461020edbb36ddd76d5a61407ae210056ea
Paulo Henrique <paulohr.abreu@gmail.com>
2020-06-09 11:48:03 -0300

    tests add balance user

---
+++ test/tests/user/deposit/addBalanceManual.js
@@ -0,0 +1,28 @@
+import {
+    modifyBalance,
+    getUserAuth
+} from '../../../methods';
+import { mochaAsync } from '../../../utils';
+import chai from 'chai';
+
+const expect = chai.expect;
+context('Add Balance Manual', async () => {
+    var app, user, admin, wallet;
+
+    before( async () =>  {
+        app = global.test.app;
+        user = global.test.user;
+        admin = global.test.admin;
+        user = (await getUserAuth({user : global.test.user.id, app: app.id}, global.test.user.bearerToken, {id : global.test.user.id})).data.message;
+        wallet = user.wallet.find((w) => w.currency.ticket== "eth");
+    });
+
+    it('should add 0.01 in balance of user', mochaAsync(async () => {
+        const res = await modifyBalance({user : user.id, app: app.id, admin: admin.id, wallet: wallet._id, newBalance: wallet.playBalance + 0.01}, admin.security.bearerToke, {id : admin.id} );
+        console.log(res);
+        user = (await getUserAuth({user : global.test.user.id, app: app.id}, global.test.user.bearerToken, {id : global.test.user.id})).data.message;
+        console.log(user.wallet);
+        expect(res.data.status).to.equal(200);
+    }));
+});
+
