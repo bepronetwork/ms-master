d24649ec9e75c7b11aa8616978f9f443f08993dc
Paulo Henique R Abreu <paulohrodriguesaifal@gmail.com>
2020-06-11 09:43:03 -0300

    addon resolve

--- src/controllers/Mapper/Structures/get.js
+++ src/controllers/Mapper/Structures/get.js
@@ -192,9 +192,9 @@ const get_object = (object) => {
         "description": object.description,
         "hosting_id": object.hosting_id,
         "web_url": object.web_url,
-        "addOn": {...object.addOn,
+        "addOn": {...object.addOn._doc,
             jackpot: {
-                ...object.addOn.jackpot,
+                ...object.addOn.jackpot._doc,
                 bets: [],
                 resultSpace: []
             }
