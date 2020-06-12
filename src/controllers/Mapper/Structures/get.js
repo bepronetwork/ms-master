6036380f5c87b803f50fc1c15980bd50b67d53b9
Paulo Henique R Abreu <paulohrodriguesaifal@gmail.com>
2020-06-12 14:49:51 -0300

    mapper jackpot

--- src/controllers/Mapper/Structures/get.js
+++ src/controllers/Mapper/Structures/get.js
@@ -159,13 +159,13 @@ const get_object = (object) => {
         "description": object.description,
         "hosting_id": object.hosting_id,
         "web_url": object.web_url,
-        "addOn": {...object.addOn._doc,
-            jackpot: {
+        "addOn": object.addOn ? {...object.addOn._doc,
+            jackpot: object.addOn.jackpot ?  {
                 ...object.addOn.jackpot._doc,
                 bets: [],
                 resultSpace: []
-            }
-        },
+            } : {}
+        } : {},
         "__v": object.__v,
     }
 }
