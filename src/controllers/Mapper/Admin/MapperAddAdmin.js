a3a74b8bb6366f7aba4db4a6c345ffd520f758db
matheusrmeloo <matheusrodrigues030717@gmail.com>
2020-07-16 23:34:07 -0300

    Fix Mapper

--- src/controllers/Mapper/Admin/MapperAddAdmin.js
+++ src/controllers/Mapper/Admin/MapperAddAdmin.js
@@ -16,12 +16,7 @@ let outputs = {
             "_id": object._id,
             "username": object.username,
             "name": object.name,
-            "security": !object.security ? {} : (object.security['2fa_set'] && object.security['bearerToken']) == undefined ? object.security : {
-                "_id": object.security._id,
-                "id": object.security._id,
-                "2fa_set": object.security['2fa_set'],
-                "email_verified": object.security.email_verified
-            },
+            ...security_object(object),
             "email": object.email,
             ...app_object(object),
             "registered": object.registered,
