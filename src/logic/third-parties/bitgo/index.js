3fd83e14ae3e07bdc20eb3d4ac611a31cdd9c9bc
matheusrmeloo <matheusrodrigues030717@gmail.com>
2020-07-31 11:34:15 -0300

    Fixing Errors on bitgo and migrationBanners name

--- src/logic/third-parties/bitgo/index.js
+++ src/logic/third-parties/bitgo/index.js
@@ -39,6 +39,7 @@ class BitGoClass {
     }
 
     async getWallet({ticker, id}){
+        var currencyTicker = `${IS_DEVELOPMENT ? 't' : ''}${new String(ticker).toLowerCase()}`;
         switch(ticker.toLowerCase()){
             case 'eth' : {
                 break;
@@ -47,11 +48,10 @@ class BitGoClass {
                 break;
             };
             default : {
-               // ticker = 'eth';
+               return await this.bitgo.coin(currencyTicker).wallets().get({id, allTokens : true});
             }
         }
-        const currencyTicker = `${IS_DEVELOPMENT ? 't' : ''}${new String(ticker).toLowerCase()}`;
-        return await this.bitgo.coin(currencyTicker).wallets().get({id, allTokens : true});
+        return await this.bitgo.coin(currencyTicker).wallets().get({id});
     }
 
     async generateDepositAddress({wallet, id, label}){
@@ -67,16 +67,17 @@ class BitGoClass {
 
     async getTransaction({id, wallet_id, ticker}){
         const wallet = await this.getWallet({ticker, id : wallet_id});
-        var res = await wallet.getTransfer({id, allTokens : true});
+        var res = await wallet.getTransfer({id});
         // Update Amount based on the type of Wei or Sats
         res.value = getCurrencyAmountFromBitGo({ticker, amount : res.value});
         return res;
     }
 
     async addAppDepositWebhook({wallet, id, currency_id}){
+        
         let res = await wallet.addWebhook({
             url: `${MS_MASTER_URL}/api/app/webhookBitgoDeposit?id=${id}&currency=${currency_id}`,
-            allToken : true,
+            //allToken : true,
             type: "transfer",
             numConfirmations : 3,
             listenToFailureStates : false
