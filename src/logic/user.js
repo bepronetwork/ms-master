5207f5ac70bc603a1006543a4792f72f8683880c
matheusrmeloo <matheusrodrigues030717@gmail.com>
2020-08-17 20:48:29 -0300

    Adding console

--- src/logic/user.js
+++ src/logic/user.js
@@ -394,6 +394,7 @@ const processActions = {
             const to    = params.payload.to;
             var isPurchase = false, virtualWallet = null, appVirtualWallet = null;
             const isValid = (params.payload.status === "0x1");
+            console.log("wallet:: ", wallet)
             console.log("wallet.bank_address:: ", wallet.bank_address)
             console.log("From:: ", from)
             if(wallet.bank_address == from){throwError('PAYMENT_FORWARDING_TRANSACTION')}
