22cd9013b5a103d6a4d532cef5a06e311532231d
Paulo <paulohr.abreu@gmail.com>
2020-08-16 22:45:38 -0300

    resolve error generate address

--- src/logic/user.js
+++ src/logic/user.js
@@ -626,10 +626,15 @@ const progressActions = {
     __getDepositAddress: async (params) => {
         const { app_wallet, user_wallet, user, app } = params;
     
-        if(!app_wallet.bank_address_not_webhook) {
-            var walletToAddress2 = await BitGoSingleton.getWallet({ ticker: app_wallet.currency.ticker, id: app_wallet.bitgo_id });
-            let bitgo_address2   = await BitGoSingleton.generateDepositAddress({ wallet : walletToAddress2, label: `${app._id}-${app_wallet.currency.ticker}`});
-            await WalletsRepository.prototype.updateAddress2(app_wallet._id, bitgo_address2.address);
+        if(app_wallet.bank_address_not_webhook!=null && app_wallet.bank_address_not_webhook!=undefined) {
+            try {
+                var walletToAddress2 = await BitGoSingleton.getWallet({ ticker: app_wallet.currency.ticker, id: app_wallet.bitgo_id });
+                let bitgo_address2   = await BitGoSingleton.generateDepositAddress({ wallet : walletToAddress2, label: `${app._id}-${app_wallet.currency.ticker}`});
+                if(!bitgo_address2.address) return;
+                await WalletsRepository.prototype.updateAddress2(app_wallet._id, bitgo_address2.address);
+            } catch(err) {
+                return;
+            }
         }
         console.log("generateDepositAddress 2 App");
 
