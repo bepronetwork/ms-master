2540e5ca15a3e9ebcfa995a14730a9332ff1c207
Paulo Henrique <paulohr.abreu@gmail.com>
2020-08-23 11:28:23 -0300

    ?ssl=true&authSource=admin&retryWrites=true&w=majority

--- src/Globals.js
+++ src/Globals.js
@@ -83,17 +83,17 @@ class Globals{
         // Main DB
         this.main_db = new Mongoose();
         this.main_db.set('useFindAndModify', false);
-        await this.main_db.connect(`${MONGO_CONNECTION_STRING}/main`, { useNewUrlParser: true, useUnifiedTopology: true});
+        await this.main_db.connect(`${MONGO_CONNECTION_STRING}/main?ssl=true&authSource=admin&retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true});
         this.main_db.Promise = bluebird;
         // Ecosystem DB
         this.ecosystem_db = new Mongoose();
         this.ecosystem_db.set('useFindAndModify', false);
-        await this.ecosystem_db.connect(`${MONGO_CONNECTION_STRING}/ecosystem`, { useNewUrlParser: true, useUnifiedTopology: true});
+        await this.ecosystem_db.connect(`${MONGO_CONNECTION_STRING}/ecosystem?ssl=true&authSource=admin&retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true});
         this.ecosystem_db.Promise = bluebird;
         // Redis DB
         this.redis_db = new Mongoose();
         this.redis_db.set('useFindAndModify', false);
-        await this.redis_db.connect(`${MONGO_CONNECTION_STRING}/redis`, { useNewUrlParser: true, useUnifiedTopology: true});
+        await this.redis_db.connect(`${MONGO_CONNECTION_STRING}/redis?ssl=true&authSource=admin&retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true});
         this.redis_db.Promise = bluebird;
         // Main DB
         this.default = new Mongoose();
