0911f93278a796ab32b3defafdd678245c35b18a
matheusrmeloo <matheusrodrigues030717@gmail.com>
2020-08-29 12:56:11 -0300

    Decrypt Hash

--- src/logic/provider.js
+++ src/logic/provider.js
@@ -34,7 +34,8 @@ const processActions = {
         let listProviders = await ProviderRepository.prototype.findByApp(params.app);
         console.log("listProviders:: ", listProviders)
         let res = listProviders.map(async (provider) => {
-            let listGames = await axios.get(`${provider.api_url}/GetListGames?partner_id=${provider.partner_id}&type=web_slot&hash=${md5("GetListGames/" + provider.partner_id + "web_slot" + provider.api_key)}`);
+            let providerKey = Security.prototype.decryptData(provider.api_key);
+            let listGames = await axios.get(`${provider.api_url}/GetListGames?partner_id=${provider.partner_id}&type=web_slot&hash=${md5("GetListGames/" + provider.partner_id + "web_slot" + providerKey)}`);
             console.log("ListGames:: ", listGames.data);
             return { name: provider.name, list: listGames.data };
         })
