f0454b6319df61a32b4ba62b861b8761543112c9
matheusrmeloo <matheusrodrigues030717@gmail.com>
2020-08-31 19:15:23 -0300

    Adding Filter to Games provider

--- src/logic/provider.js
+++ src/logic/provider.js
@@ -33,11 +33,15 @@ const processActions = {
         return {...providerEco, app: params.app, providerEco: params.provider_id};
     },
     __getGamesProvider: async (params) => {
-        let listProviders = await ProviderRepository.prototype.findByApp(params.app);
+        let listProviders = await ProviderRepository.prototype.findByApp({
+            app: params.app,
+            providerEco: !params.providerEco ? {} : { providerEco: params.providerEco }
+        });
+        console.log("listProviders:: ", listProviders)
         let res = listProviders.map(async (provider) => {
             let providerKey = Security.prototype.decryptData(provider.api_key);
             let listGames = await axios.get(`${provider.api_url}/GetListGames?partner_id=${provider.partner_id}&type=web_slot&hash=${md5("GetListGames/" + provider.partner_id + "web_slot" + providerKey)}`);
-            return { name: provider.name, api_url: provider.api_url , list: listGames.data };
+            return { name: provider.name, api_url: provider.api_url, partner_id: provider.partner_id , list: listGames.data };
         })
         return await Promise.all(res);
     }
