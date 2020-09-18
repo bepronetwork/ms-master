88927269e07e62505996f0d81f21cdc86aa97733
matheusrmeloo <matheusrodrigues030717@gmail.com>
2020-09-18 13:53:45 -0300

    Remove consoles

--- test/tests/user/points/index.js
+++ test/tests/user/points/index.js
@@ -18,11 +18,8 @@ context('Points', async () => {
         currency = app.currencies[0]._id,
         await UsersRepository.prototype.updateUserPoints({ _id: user.id, value: 1})
         user = await UsersRepository.prototype.findUserById(user.id);
-        console.log("user:: ", user)
         userWalletBefore = user.wallet.find( w => new String(w.currency._id).toLowerCase() == new String(currency).toLowerCase());
-        console.log("userWalletBefore:: ", userWalletBefore)
         points = user.points
-        console.log("points:: ", points)
     });
 
     it('should Convert Points To Currency', mochaAsync(async () => {
@@ -35,9 +32,7 @@ context('Points', async () => {
         let res = await convertPoints({...postData, admin: admin.id}, admin.security.bearerToken , {id : admin.id});
         expect(res.data.status).to.equal(200);
         userAfter = await UsersRepository.prototype.findUserById(user._id);
-        console.log("userAfter:: ", userAfter)
         userWalletAfter = userAfter.wallet.find( w => new String(w.currency._id).toLowerCase() == new String(currency).toLowerCase());
-        console.log("userWalletAfter:: ", userWalletAfter)
         expect(userWalletAfter.playBalance).to.equal(userWalletBefore.playBalance + points);
         expect(userAfter.points).to.equal(0);
     }));
