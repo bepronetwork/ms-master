2c62f9ab3c94fba7b9a23bb7d4472122738b20c0
Paulo <paulohr.abreu@gmail.com>
2020-10-15 16:19:50 -0300

    remove console

--- src/logic/customization.js
+++ src/logic/customization.js
@@ -75,8 +75,6 @@ const progressActions = {
 				}]
 			})).register())._doc._id;
 
-			console.log(topBar," 1 ", banners," 2 ", subSections," 3 ", footer," 4 ", topTab)
-
             let customization = await self.save({...params, languages, topBar, banners, subSections, footer, topTab});
 			return {
 				...customization,
