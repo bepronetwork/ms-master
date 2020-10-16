7aee47b8759d6eead39546aa43da8969780def36
Paulo <paulohr.abreu@gmail.com>
2020-10-16 14:27:26 -0300

    === true

--- src/logic/utils/language.js
+++ src/logic/utils/language.js
@@ -2,7 +2,7 @@ class Language {
     filterLanguageEN(arrayToMap) {
         let objEN = arrayToMap.find((res)=>res.language.prefix.toUpperCase()=="EN");
         let list = arrayToMap.map((obj)=>{
-            let res = (obj.useStandardLanguage == true) ? objEN : obj ;
+            let res = (obj.useStandardLanguage === true) ? objEN : obj ;
             return {...res, language: obj.language};
         });
         return list;
