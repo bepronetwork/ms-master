00d7eaaef8874c93680cc56846f63a745e303dce
Paulo <paulohr.abreu@gmail.com>
2020-10-16 14:25:22 -0300

    resolve language customization

--- src/logic/utils/language.js
+++ src/logic/utils/language.js
@@ -3,7 +3,7 @@ class Language {
         let objEN = arrayToMap.find((res)=>res.language.prefix.toUpperCase()=="EN");
         let list = arrayToMap.map((obj)=>{
             let res = (obj.useStandardLanguage == true) ? objEN : obj ;
-            return res;
+            return {...res, language: obj.language};
         });
         return list;
     }
