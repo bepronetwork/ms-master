018a0b8ef36504fd5a25860c132a67c44be3be41
Rui Pedro <ruivaz1997@gmail.com>
2020-10-19 12:43:43 +0100

    Revert "Hotfix customization"

--- src/logic/utils/language.js
+++ src/logic/utils/language.js
@@ -2,8 +2,8 @@ class Language {
     filterLanguageEN(arrayToMap) {
         let objEN = arrayToMap.find((res)=>res.language.prefix.toUpperCase()=="EN");
         let list = arrayToMap.map((obj)=>{
-            let res = (obj.useStandardLanguage === true) ? objEN : obj ;
-            return {...res, language: obj.language};
+            let res = (obj.useStandardLanguage == true) ? objEN : obj ;
+            return res;
         });
         return list;
     }
