3086ecf9c020a182d2e1b2a1217c24660a27c8f5
Paulo <paulohr.abreu@gmail.com>
2020-10-19 09:32:09 -0300

    Convert from object js to object JSON

--- src/logic/utils/language.js
+++ src/logic/utils/language.js
@@ -2,7 +2,9 @@ class Language {
     filterLanguageEN(arrayToMap) {
         let objEN = arrayToMap.find((res)=>res.language.prefix.toUpperCase()=="EN");
         let list = arrayToMap.map((obj)=>{
-            let res = (obj.useStandardLanguage === true) ? objEN : obj ;
+            let res = (obj.useStandardLanguage == true) ? objEN : obj ;
+                // Convert from object js to object JSON
+                res = JSON.parse(JSON.stringify(res));
             return {...res, language: obj.language};
         });
         return list;
