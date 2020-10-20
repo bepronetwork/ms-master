7d1ee54aed24dd1f8d45992342c46f1f8f823afc
matheusrmeloo <matheusrodrigues030717@gmail.com>
2020-10-20 00:36:19 -0300

    Fix Json of Test

--- test/tests/betscan/index.js
+++ test/tests/betscan/index.js
@@ -13,7 +13,7 @@ const expect = chai.expect;
 context('Betscan', async () => {
 
     it('should Get Bets Casino for Betscan', mochaAsync(async () => {
-        let res = await betscanGetBets();
+        let res = await betscanGetBets({});
         console.log("Res:: ",res)
         console.log("ResDATA:: ",res.data)
         expect(res.data.status).to.not.null;
@@ -21,25 +21,25 @@ context('Betscan', async () => {
     }));
 
     it('should Get Bets Esports for Betscan', mochaAsync(async () => {
-        let res = await betscanGetBetsEsports();
+        let res = await betscanGetBetsEsports({});
         expect(res.data.status).to.not.null;
         expect(res.data.status).to.equal(200);
     }));
 
     it('should Get Users for Betscan', mochaAsync(async () => {
-        let res = await betscanGetUsers();
+        let res = await betscanGetUsers({});
         expect(res.data.status).to.not.null;
         expect(res.data.status).to.equal(200);
     }));
 
     it('should Get Deposits for Betscan', mochaAsync(async () => {
-        let res = await betscanGetDeposits();
+        let res = await betscanGetDeposits({});
         expect(res.data.status).to.not.null;
         expect(res.data.status).to.equal(200);
     }));
 
     it('should Get Withdraws for Betscan', mochaAsync(async () => {
-        let res = await betscanGetWithdraws();
+        let res = await betscanGetWithdraws({});
         expect(res.data.status).to.not.null;
         expect(res.data.status).to.equal(200);
     }));
